16a0e73b342728a2c05537bf96adcbb5ff312fe9
Marlon Johnson <johnsonmarlon18@gmail.com>
2020-08-18 15:38:36 -0700

    Create UI for Home Page (#7)
    * 3/4 of the home page is complete
    
    * completed home page ui
    
    * feat(ui/ux): implemented tailwind css #6

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  important: true,
+  purge: ['./public/*.html', './src/**/*.js', './src/**/*.jsx'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      display: ['Lato', 'sans-serif'],
+      body: ['Lato', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: [],
+}
