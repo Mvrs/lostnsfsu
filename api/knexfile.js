2ff06c6430bebb79b2252ac88a732755c9627e63
Marlon Johnson <johnsonmarlon18@gmail.com>
2020-08-31 02:32:04 -0700

    docker-container setup #5

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,31 +1,32 @@
+require('dotenv').config()
+
 if (process.env.NODE_ENV !== 'production')
-  require('dotenv').config({ path: './local.env' })
+	require('dotenv').config({ path: './local.env' })
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      user: 'postgres',
-      database: 'lostinsfsu-api',
-      host: 'localhost',
-      password: 'postgres'
-    },
-    migrations: {
-      directory: __dirname + '/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/seeds'
-    }
-  },
-  // production is in the works
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: __dirname + '/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/seeds/production'
-    }
-  }
+	development: {
+		client: 'pg',
+		connection: {
+			user: process.env.POSTGRES_USER,
+			database: process.env.POSTGRES_DB,
+			password: process.env.POSTGRES_PASSWORD,
+		},
+		migrations: {
+			directory: __dirname + '/migrations',
+		},
+		seeds: {
+			directory: __dirname + '/seeds',
+		},
+	},
+	// production is in the works
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: __dirname + '/migrations',
+		},
+		seeds: {
+			directory: __dirname + '/seeds/production',
+		},
+	},
 }
